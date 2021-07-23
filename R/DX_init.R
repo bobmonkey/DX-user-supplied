136dc6e3cb0c8712dd12afd6df9649b9d7f3cf08
Jon McCurdy <jonmccurdy@ymail.com>
2021-07-23 13:56:00 -0500

    altered parameter input

--- R/DX_init.R
+++ R/DX_init.R
@@ -1,6 +1,3 @@
-dx_init <- function(seed=12345, K_X1=47, B_X1=536869888) {
-  set.seed(seed)
-  .Random.seed[3] <<- K_X1
-  .Random.seed[4] <<- B_X1
-  .Random.seed <<- as.integer(.Random.seed)
+dx_init <- function(K_X1=47) {
+  dx_k_value <<- K_X1
 }
--- src/C_code_user.cpp
+++ src/C_code_user.cpp
@@ -7,18 +7,23 @@ using namespace Rcpp;
 #define K_MAX	3500
 #define PP 2147483647  //2^31-1
 #define B_X1 536869888 
-#define K_X1 47
 #define T_X1 1
 
 unsigned long MODP(unsigned long z) {return ((((z)&PP)+((z)>>31)) &PP);}
 
 static int I_X, K_X, T_X, B_X;
+NumericVector K_X1;
 
 static Int32 XX[47];
 static Int32 seed_in;
-static Int32 seed[3];
 static double res;
+static Int32 seed[3];
 static int nseed = 3;
+
+void parameter(){
+   Environment env = Environment::global_env();
+   if(env["dx_k_value"]==R_NilValue) {K_X1=47;} else {K_X1=env["dx_k_value"];}
+}
 
 void DX_Init(Int32 seed_in)
 {
@@ -30,19 +35,9 @@ void DX_Init(Int32 seed_in)
   B_X=seed[2];
   for (i=0; i<K_X; i++) XX[i] = rand() & PP;
   I_X=K_X-1;
-  
-  seed[0]=0;
 }
 
-double * user_unif_rand()
-{
-  if(seed[1] == 0 || seed[2] == 0){
-    seed[1]=47; seed[2]=536869888;
-  }
-  if(seed[0] != 0){
-    DX_Init((unsigned int)seed[0]);
-  }
-  
+double * user_unif_rand() {
   int II0 = I_X;
   if(++I_X >= K_X)  I_X = 0;     
   XX[I_X] = MODP(B_X * XX[I_X] + XX[II0]);
@@ -51,7 +46,11 @@ double * user_unif_rand()
 }
 
 void  user_unif_init(Int32 seed_in) {
+  parameter();
   seed[0] = seed_in;
+  seed[1]=K_X1[0]; 
+  seed[2]=536869888;
+  DX_Init((unsigned int)seed[0]);
 }
 
 int * user_unif_nseed() { return &nseed; }
